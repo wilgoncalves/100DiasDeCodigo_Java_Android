b315942addb73172bc9444bc5cfa50f63025804a
Willian Gonçalves <santoswil81@gmail.com>
2025-10-31 19:23:38 -0300

    #100diasdecodigo: criando interface para app da Netflix

--- #100DiasDeCodigo/NetflixInterface/app/src/main/java/com/williangoncalves/netflixinterface/MainActivity.java
+++ #100DiasDeCodigo/NetflixInterface/app/src/main/java/com/williangoncalves/netflixinterface/MainActivity.java
@@ -1,8 +1,9 @@
-package com.williangoncalves.constraintlayout;
+package com.williangoncalves.netflixinterface;
 
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Netflix");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
