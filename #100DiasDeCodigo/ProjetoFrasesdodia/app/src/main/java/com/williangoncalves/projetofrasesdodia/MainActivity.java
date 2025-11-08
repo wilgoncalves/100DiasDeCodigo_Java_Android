0a91c236d0dfe4326116ed8502b75d06ee6f4901
Willian Gonçalves <santoswil81@gmail.com>
2025-11-07 22:46:07 -0300

    #100diasdecodigo: criando interface para app frases do dia

---
+++ #100DiasDeCodigo/ProjetoFrasesdodia/app/src/main/java/com/williangoncalves/projetofrasesdodia/MainActivity.java
@@ -0,0 +1,25 @@
+package com.williangoncalves.projetofrasesdodia;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        getSupportActionBar().setTitle("Frases do dia");
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
