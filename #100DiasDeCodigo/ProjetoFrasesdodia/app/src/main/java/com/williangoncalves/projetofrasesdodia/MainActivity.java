c340df16ff91986a8ebfcad06173243a6e2598c0
Willian Gonçalves <santoswil81@gmail.com>
2025-11-10 21:52:52 -0300

    #100diasdecodigo: finalizando app frases do dia

--- #100DiasDeCodigo/ProjetoFrasesdodia/app/src/main/java/com/williangoncalves/projetofrasesdodia/MainActivity.java
+++ #100DiasDeCodigo/ProjetoFrasesdodia/app/src/main/java/com/williangoncalves/projetofrasesdodia/MainActivity.java
@@ -1,12 +1,16 @@
 package com.williangoncalves.projetofrasesdodia;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void gerarNovaFrase(View view) {
+
+        String[] frases = {
+                "Frase 1",
+                "Frase 2",
+                "Frase 3",
+                "Frase 4"
+        };
+
+        int numero = new Random().nextInt(4);
+
+        TextView texto = findViewById(R.id.textResultado);
+        texto.setText(frases[numero]);
     }
 }
