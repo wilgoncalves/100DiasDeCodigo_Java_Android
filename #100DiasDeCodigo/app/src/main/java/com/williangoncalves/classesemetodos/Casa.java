a87cbf141a30db9aa8cab84005c452026db02b41
Willian Gonçalves <santoswil81@gmail.com>
2025-11-12 22:50:00 -0300

    Classes e objetos no android studio

---
+++ #100DiasDeCodigo/app/src/main/java/com/williangoncalves/classesemetodos/Casa.java
@@ -0,0 +1,12 @@
+package com.williangoncalves.classesemetodos;
+
+public class Casa {
+
+    // propriedades
+    String cor;
+
+    // métodos
+    void abrirPorta() {
+        System.out.println("Porta aberta!");
+    }
+}
