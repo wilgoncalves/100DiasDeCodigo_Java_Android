0b98673e0f97172bb1e75540d04b268bf56a39e7
Willian Gonçalves <santoswil81@gmail.com>
2025-11-19 15:25:13 -0300

    recuperando valores de caixas de texto

--- #100DiasDeCodigo/ComponentesBasicos/app/src/main/java/com/williangoncalves/componentesbasicos/MainActivity.java
+++ #100DiasDeCodigo/ComponentesBasicos/app/src/main/java/com/williangoncalves/componentesbasicos/MainActivity.java
@@ -1,6 +1,9 @@
 package com.williangoncalves.componentesbasicos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText campoNome;
+    private TextInputEditText campoEmail;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        campoNome = findViewById(R.id.editNome);
+        campoEmail = findViewById(R.id.editEmail);
+        textoResultado = findViewById(R.id.textResultado);
+    }
+
+    public void enviar(View view) {
+
+        String nome = campoNome.getText().toString();
+        String email = campoEmail.getText().toString();
+        textoResultado.setText(getString(R.string.resultado, nome, email));
+    }
+
+    public void limpar(View view) {
+
+        campoNome.setText("");
+        campoEmail.setText("");
     }
 }
