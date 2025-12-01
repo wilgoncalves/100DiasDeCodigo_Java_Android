3bd2331fd6b30aabb24bff250ce57ddc39de965c
Willian Gonçalves <santoswil81@gmail.com>
2025-12-01 09:17:02 -0300

    Finalizando app Alcool ou Gasolina

--- #100DiasDeCodigo/AlcoolOuGasolina/app/src/main/java/com/williangoncalves/alcoolougasolina/MainActivity.java
+++ #100DiasDeCodigo/AlcoolOuGasolina/app/src/main/java/com/williangoncalves/alcoolougasolina/MainActivity.java
@@ -1,6 +1,8 @@
 package com.williangoncalves.alcoolougasolina;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editPrecoAlcool, editPrecoGasolina;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
+        editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
+        textResultado = findViewById(R.id.textResultado);
+    }
+
+    public void calcularPreco(View view) {
+
+        String precoAlcool = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+
+        Boolean camposValidados = validarCampos(precoAlcool, precoGasolina);
+        if (camposValidados) {
+
+            Double valorAlcool = Double.parseDouble(precoAlcool);
+            Double valorGasolina = Double.parseDouble(precoGasolina);
+
+            double valorResultado = valorAlcool / valorGasolina;
+
+            if (valorResultado >= 0.7) {
+                textResultado.setText("É melhor utilizar gasolina!");
+            } else {
+                textResultado.setText("É melhor utilizar álcool!");
+            }
+
+        } else {
+            textResultado.setText("Preencha todos os campos!");
+        }
+    }
+
+    public Boolean validarCampos(String precoAlcool, String precoGasolina) {
+
+        Boolean camposValidados = true;
+
+        if (precoAlcool == null || precoAlcool.equals("")) {
+            camposValidados = false;
+        } else if (precoGasolina == null || precoGasolina.equals("")) {
+            camposValidados = false;
+        }
+
+        return camposValidados;
     }
 }
