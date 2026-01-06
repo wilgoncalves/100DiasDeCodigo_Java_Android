3e0943996df6a4fe929c953484740915a3085cc4
Willian Gonçalves <santoswil81@gmail.com>
2026-01-06 11:26:07 -0300

    Passando dados entre activities

--- #100DiasDeCodigo/Passandodadosactivities/app/src/main/java/com/williangoncalves/passandodadosactivities/MainActivity.java
+++ #100DiasDeCodigo/Passandodadosactivities/app/src/main/java/com/williangoncalves/passandodadosactivities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.williangoncalves.passandodadosactivities;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,7 +31,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
 
+                // Instanciar o objeto:
+                Usuario usuario = new Usuario("Willian", "willian@gmail.com");
+
+                // Passar dados:
+                intent.putExtra("nome", "Willian");
+                intent.putExtra("idade", 27);
+                intent.putExtra("objeto", usuario);
+
+                startActivity(intent);
             }
         });
     }
---
+++ #100DiasDeCodigo/Passandodadosactivities/app/src/main/java/com/williangoncalves/passandodadosactivities/SegundaActivity.java
@@ -0,0 +1,40 @@
+package com.williangoncalves.passandodadosactivities;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    private TextView textViewNome, textViewIdade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_segunda);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textViewNome = findViewById(R.id.textViewNome);
+        textViewIdade = findViewById(R.id.textViewIdade);
+
+        // Recuperar dados enviados:
+        Bundle dados = getIntent().getExtras();
+        String nome = dados.getString("nome");
+        int idade = dados.getInt("idade");
+        Usuario usuario = (Usuario) dados.getSerializable("objeto");
+
+        // Configurar valores recuperados:
+        textViewNome.setText(usuario.getEmail());
+        textViewIdade.setText(String.valueOf(idade));
+    }
+}
