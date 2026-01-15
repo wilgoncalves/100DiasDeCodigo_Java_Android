bf6e4fcd3f205d33126f1862a1b606e12228be6b
Willian Gonçalves <santoswil81@gmail.com>
2026-01-15 15:17:18 -0300

    Utilizando Navigation Drawer

--- #100DiasDeCodigo/NavigationDrawer/app/src/main/java/com/williangoncalves/navigationdrawer/MainActivity.java
+++ #100DiasDeCodigo/NavigationDrawer/app/src/main/java/com/williangoncalves/navigationdrawer/MainActivity.java
@@ -39,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Define configurações do NavigationDrawer:
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_contact, R.id.nav_share, R.id.nav_send)
                 .setOpenableLayout(drawer)
                 .build();
 
