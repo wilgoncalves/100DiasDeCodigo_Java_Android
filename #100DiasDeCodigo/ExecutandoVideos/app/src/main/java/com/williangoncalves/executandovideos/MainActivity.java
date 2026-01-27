a1bee6a8df5f27a7400c3ee97d6ee1b36259073d
Willian Gonçalves <santoswil81@gmail.com>
2026-01-27 15:50:06 -0300

    executando videos em aplicativo

--- #100DiasDeCodigo/ExecutandoVideos/app/src/main/java/com/williangoncalves/executandovideos/MainActivity.java
+++ #100DiasDeCodigo/ExecutandoVideos/app/src/main/java/com/williangoncalves/executandovideos/MainActivity.java
@@ -1,6 +1,8 @@
 package com.williangoncalves.executandovideos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void abrirVideo(View view) {
+
+        startActivity(new Intent(this, PlayerActivity.class));
     }
 }
---
+++ #100DiasDeCodigo/ExecutandoVideos/app/src/main/java/com/williangoncalves/executandovideos/PlayerActivity.java
@@ -0,0 +1,44 @@
+package com.williangoncalves.executandovideos;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_player);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        videoView = findViewById(R.id.videoView);
+
+        // Esconder a ActionBar:
+        //etSupportActionBar().hide(); ~ não funcionou ~
+
+        // Esconder a statusBar e barra de navegação:
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        // Executar o vídeo:
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+        videoView.start();
+    }
+}
