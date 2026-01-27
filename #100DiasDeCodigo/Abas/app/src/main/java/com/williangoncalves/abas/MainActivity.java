54eaa1b70763b988cf88faef57a2a8e91e63f816
Willian Gonçalves <santoswil81@gmail.com>
2026-01-27 15:50:37 -0300

    Construindo abas em aplicativos

---
+++ #100DiasDeCodigo/Abas/app/src/main/java/com/williangoncalves/abas/MainActivity.java
@@ -0,0 +1,49 @@
+package com.williangoncalves.abas;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+import com.williangoncalves.abas.Fragment.EmAltaFragment;
+import com.williangoncalves.abas.Fragment.HomeFragment;
+import com.williangoncalves.abas.Fragment.InscricoesFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        // Configurar adapter para abas:
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Home", HomeFragment.class)
+                .add("Inscrições", InscricoesFragment.class)
+                .add("Em Alta", EmAltaFragment.class)
+                .create());
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
